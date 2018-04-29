12325a93e1b99edc52f58ca7df8d949f0200883e
Oliver Cahlander <Jesters@Olivers-MacBook-Air.local>
2018-04-29 04:48:27 -0700

    test to get list of todos

--- NodeToDoApi/server/tests/server.test.js
+++ NodeToDoApi/server/tests/server.test.js
@@ -7,10 +7,16 @@ var {mongoose,db} = require('../mongoose');
 
 var Todo = mongoose.model('Todo', todoSchema);
 
+const dummyTodos = [{
+    text: "First Test TODo"        
+    }, {
+    text: "2nd Test TODo"        
+}];
+
 beforeEach((done) => {
     Todo.remove({}).then(() => {
-        done();
-    });
+        return Todo.insertMany(dummyTodos);
+    }).then(()=> {done();});
 });
 
 describe('POST /todos', () => {
@@ -30,7 +36,7 @@ describe('POST /todos', () => {
                return done(err);
            }
            
-           Todo.find().then((todos) => {
+           Todo.find({text}).then((todos) => {
                expect(todos.length).toBe(1);
                expect(todos[0].text).toBe(text);
                console.log(JSON.stringify(todos, undefined,2));
@@ -51,9 +57,21 @@ describe('POST /todos', () => {
            }
            
            Todo.find().then((todos) => {
-               expect(todos.length).toBe(0);
+               expect(todos.length).toBe(2);
                done();
            }).catch((e) => done(e));
        });
    }); 
+});
+
+describe('GET /todos', () => {
+    it('should list all todo', (done) => { 
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todos.length).toBe(2);
+        });     
+        done();
+    });
 });
